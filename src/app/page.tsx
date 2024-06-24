8590ec82556506f28066233b230b9f3be6aadb51
Thien <ubw1212@gmail.com>
2024-06-25 03:19:51 +0700

    Add header, sidebar, toolbar, pages (canvas); add function to add shapes to the page; add the 'add page' button to add more page...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
-import Image from "next/image";
+'use client'
+import CanvasContainer from '@/components/canvas-container'
+import {Sidebar} from "@/components/sidebar";
+import {useState} from "react";
+import {usePages} from '../context/page-context'
 
 export default function Home() {
-  return (
-    <></>
-  );
+    const [isExpanded, setIsExpanded] = useState(false);
+    const {setCurrentPage} = usePages()
+
+
+    const resetCurrentPage: React.MouseEventHandler<HTMLDivElement> = (e) => {
+        e.stopPropagation()
+        setCurrentPage(0)
+        console.log('clicked')
+    }
+
+    const canvasStylePadding = {marginLeft: isExpanded ? '352px' : ''}
+
+    return (
+
+        <div className='w-screen h-screen flex'>
+            <Sidebar isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
+            <div className='w-full pt-32 pl-20 bg-gray-200'>
+                <div className='fixed top-16 w-full h-16 bg-white border-b-0.5'
+                     style={canvasStylePadding}
+                >
+                    Toolbar
+                </div>
+                <div className={`flex justify-center items-start pt-28 h-full border-gray-400`}
+                     style={canvasStylePadding}
+                     onClick={resetCurrentPage}
+                >
+                    <CanvasContainer/>
+                </div>
+            </div>
+        </div>
+
+    )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      borderWidth: {
+        '0.5': '0.5px',
+      },
+      width: {
+        '1640': '1640px',
+        '88': '352px'
+      },
+      height: {
+        '924': '924px'
+      },
+      border: {
+
+      },
+      padding: {
+        '28px': '28px',
+        '432px': '432px'
+      },
+      fontSize: {
+        '10px': '10px',
+        '8px': '8px',
+        '6px': '6px',
+        '4px': '4px',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-gradient": "linear-gradient(90deg, rgba(102,83,168,1) 0%, rgba(3,221,171,1) 49%, rgba(0,212,255,1) 100%)"
       },
     },
   },
