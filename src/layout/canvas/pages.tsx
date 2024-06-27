f5f92d1931981486760d08e41290a51a62ecf2a0
Thien <ubw1212@gmail.com>
2024-06-28 03:11:30 +0700

    Remove unused condition in pages.tsx
    An unused condition checking for 'item' was found in the pages.tsx file. This condition was removed to clean up the file and improve readability. This does not affect the overall functionality of the file.

--- src/layout/canvas/pages.tsx
+++ src/layout/canvas/pages.tsx
@@ -24,9 +24,6 @@ const Pages = () => {
                     y: prevState.y + offset.y,
                 }));
             }
-            if (item) {
-                item
-            }
         }
     }))
 
