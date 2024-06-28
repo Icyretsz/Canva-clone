71fd2e56dfd1cdc71716216e75091d4aa435972e
Thien <ubw1212@gmail.com>
2024-06-29 01:28:29 +0700

    Remove hover effects in page layout
    The hover effects, including the state and related event handlers, have been removed from the `Pages` component. The visual representation of the current page now solely depends on the `currentPage` state, simplifying both the code and the user interaction.

--- src/layout/canvas/pages.tsx
+++ src/layout/canvas/pages.tsx
@@ -38,17 +38,8 @@ const initialElementState: Element = {
 
 const Pages = () => {
     const {pages, currentPage, setCurrentPage, setSelectedElement} = usePages()
-    const [pageHovered, setPageHovered] = useState<number>(0)
 
-    const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>, pageNo: number) => {
-        e.stopPropagation();
-        setPageHovered(pageNo)
-    }
 
-    const handleMouseLeave: React.MouseEventHandler<HTMLDivElement> = (e) => {
-        e.stopPropagation();
-        setPageHovered(0)
-    }
 
     const handleClickOnPage = (pageNo: number, e: React.MouseEvent<HTMLDivElement>) => {
         e.stopPropagation();
@@ -60,30 +51,27 @@ const Pages = () => {
         width: page.width,
         height: page.height,
         backgroundColor: page.backgroundColor,
-        border: (page.pageNo === currentPage || pageHovered === page.pageNo) ? '2px solid black' : '',
+        border: page.pageNo === currentPage ? '2px solid black' : '',
     });
 
 
     return (
         <div className='flex flex-col justify-center gap-5'>
             {pages.map((page: Page) => (
-                <PageComponent key={page.pageNo} page={page} currentPage={currentPage} pageHovered={pageHovered} getPageStyle={getPageStyle} handleClickOnPage={handleClickOnPage} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave}/>
+                <PageComponent key={page.pageNo} page={page} currentPage={currentPage} getPageStyle={getPageStyle} handleClickOnPage={handleClickOnPage}/>
             ))}
         </div>
     );
 };
 
     interface PageComponentProp {
-        page : Page
-        currentPage : number
-        pageHovered : number
-        getPageStyle: (page :Page) => React.CSSProperties;
+        page: Page
+        currentPage: number
+        getPageStyle: (page: Page) => React.CSSProperties;
         handleClickOnPage: (pageNo: number, e: React.MouseEvent<HTMLDivElement>) => void
-        handleMouseEnter: (e: React.MouseEvent<HTMLDivElement>, pageNo: number) => void
-        handleMouseLeave: (e: React.MouseEvent<HTMLDivElement>) => void
     }
 
-const PageComponent = ({page, currentPage, pageHovered, getPageStyle, handleClickOnPage, handleMouseEnter, handleMouseLeave} : PageComponentProp) => {
+const PageComponent = ({page, currentPage, getPageStyle, handleClickOnPage} : PageComponentProp) => {
     const [elementPositions, setElementPositions] = useState<ElementPositions>({});
     const [{canDrop, isOver}, drop] = useDrop<DragItem, unknown, { canDrop: boolean; isOver: boolean }>(() => ({
         accept: `element-${page.pageNo}`,
@@ -119,12 +107,9 @@ const PageComponent = ({page, currentPage, pageHovered, getPageStyle, handleClic
     return (
         <div ref={ref} key={page.pageNo} className={`shadow-md cursor-pointer relative
                 ${page.pageNo === currentPage ? 'border-2 border-black' : 'border-2 border-transparent'}
-                ${pageHovered === page.pageNo ? 'border-2 border-black' : 'border-2 border-transparent'}
                 `}
              style={getPageStyle(page)}
              onClick={(e) => handleClickOnPage(page.pageNo, e)}
-             onMouseEnter={(e) => handleMouseEnter(e, page.pageNo)}
-             onMouseLeave={handleMouseLeave}
         >
             <Elements page={page} elementPositions={elementPositions}/>
         </div>
